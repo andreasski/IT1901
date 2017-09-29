77bd67ca5b06d05af12e71dd302e14d198ec58b1
Magnus Gaustad <magnuge@stud.ntnu.no>
2017-09-29 14:57:29 +0200

    laget manager

---
+++ IT1901FestivalApp/src/backend/Manager.java
@@ -0,0 +1,33 @@
+package backend;
+
+/**
+ * One method for adding techneeds.
+ */
+
+public class Manager {
+
+
+    /* MÅ:
+    void setTechNeeds
+    * @param: String need
+    *
+    * Send bandid(fra manager) and string needs.
+    */
+    public void setTechNeeds(String need){
+
+    }
+
+    /*
+    @param: String what to change, String change to
+     */
+
+    public void setKeyInformation(){
+
+    }
+
+    public void editKeyInformation(){
+
+    }
+
+
+}
