4973ba92064c2b383c59e715b6c5e7093f4d6d9a
Heidibr <heidilb@stud.ntnu.no>
2017-10-03 10:49:56 +0200

    bookingansvarlig

--- IT1901FestivalApp/src/backend/Bookingansvarlig.java
+++ IT1901FestivalApp/src/backend/Bookingansvarlig.java
@@ -1,3 +1,5 @@
+package backend;
+
 import backend.ConnectionManager;
 
 import java.sql.ResultSet;
@@ -8,6 +10,7 @@ public class Bookingansvarlig {
 
     ArrayList<String> bands = new ArrayList<>();
     ArrayList<String> needs = new ArrayList<>();
+    String info = "";
 
     public ArrayList<String> searchBands(String band){
 
@@ -42,5 +45,37 @@ public class Bookingansvarlig {
         return needs;
 
     }
+    public String getInfoBand( String band) {
+        try {
+            Statement stmt = new ConnectionManager().connect().createStatement();
+            ResultSet rs;
+
+            rs = stmt.executeQuery("SELECT popularity, salesalbum, salesconcerts FROM band where band.name = \"" +band+ "\"");
+            while (rs.next()) {
+                int pop = rs.getInt("popularity");
+                int sale = rs.getInt("salesalbum");
+                int conc = rs.getInt("salesconcerts");
+                String pops = Integer.toString(pop);
+                String sales =Integer.toString(sale);
+                String concs =Integer.toString(conc);
+                info += ("Popularitet: " +pops + " Albumsalg: " +sales + " Konsert salgstall: "+concs);
+            }
+        } catch (Exception e) {
+            System.err.println("Got an exception2! ");
+            System.err.println(e.getMessage());
+        }
+        return info;
+    }
+    public static void main(String[] args){
+        Bookingansvarlig test = new Bookingansvarlig();
+        String infoting = test.getInfoBand("bølgeband");
+        System.out.println(infoting);
+
+
+
+    }
+
+
+
 
 }
