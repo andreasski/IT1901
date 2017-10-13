befe04a6af20f70b1cecfa3dcb6e86db48927a1b
John Gullaksen <zhashade@gmail.com>
2017-10-13 14:25:08 +0200

    Lagt til Bookingsjef klasse

---
+++ IT1901FestivalApp/src/backend/BookingBoss.java
@@ -0,0 +1,25 @@
+package backend;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookingBoss {
+
+    BookingBoss() {
+        ConnectionManager.connect();
+    }
+
+    public List<String> getConcerts() {
+        List<String> ls = new ArrayList<String>();
+
+
+        return ls;
+    }
+
+    public String getConcert(int concertId) {
+        String res = "";
+        
+
+        return  res;
+    }
+}
