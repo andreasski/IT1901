184e9b83bdfec4939170507adc6add26fe69f9a3
Magnus Gaustad <magnuge@stud.ntnu.no>
2017-10-27 16:01:06 +0200

    prres frontend

--- IT1901FestivalApp/src/frontend/GUI/PRres_Controller.java
+++ IT1901FestivalApp/src/frontend/GUI/PRres_Controller.java
@@ -1,4 +1,28 @@
 package frontend.GUI;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+
+import java.util.ArrayList;
+
 public class PRres_Controller {
+
+    private final String INSTRUCTION_LABEL = "En herlig oversikt over konserter"; //til
+    
+    private VBox instructionBoxContainer;
+
+    private backend.PRres prres;
+
+    private VBox contents = new VBox();
+
+
+
+    public void getConcertDetails(){
+        ArrayList<String> bookedBands = prres.getBookedBands();
+        for(int i = 0; i<bookedBands.size(); i++){
+
+        }
+
+    }
+
 }
--- IT1901FestivalApp/src/frontend/GUI/PRres_Main.java
+++ IT1901FestivalApp/src/frontend/GUI/PRres_Main.java
@@ -1,4 +1,25 @@
 package frontend.GUI;
 
-public class PRres_Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class PRres_Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("../fxml/PRres_landing.fxml"));
+        primaryStage.setTitle("FestivalArrangør  | PR ansvarlig");
+        Scene scene = new Scene(root, 800, 600);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
